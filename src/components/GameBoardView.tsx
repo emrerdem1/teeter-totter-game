2e183d2e2ded04c38620a257d041b1718b569c3d
emre erdem <emrerdem94@gmail.com>
2021-06-04 23:51:58 +0300

    Move the balancer to its own component

--- src/components/GameBoardView.tsx
+++ src/components/GameBoardView.tsx
@@ -15,9 +15,8 @@ import {
   MAX_ITEM_SIZE,
 } from '../redux/constants';
 import ItemView from './ItemView';
+import BalancerView from './BalancerView';
 
-const TOTTER_LINE_HEIGHT = 5;
-const TOTTER_BALANCER_HEIGHT = 100;
 const BOARD_WIDTH = MAX_ITEM_SIZE * HORIZONTAL_CELLS_COUNT * 2;
 const BOARD_HEIGHT = MAX_ITEM_SIZE * VERTICAL_CELLS_COUNT;
 
@@ -27,48 +26,6 @@ const GameBoardDiv = styled.div`
   display: flex;
   position: relative;
   flex-wrap: wrap;
-`;
-
-const TotterBaseDiv = styled.div`
-  position: absolute;
-  height: 100%;
-  width: 100%;
-  display: flex;
-  flex-wrap: wrap;
-  align-self: flex-end;
-  justify-content: center;
-`;
-
-const TotterLineDiv = styled.div<{ torque: number }>`
-  width: 100%;
-  height: 100%;
-  transform: rotate(${(props) => props.torque ?? '0'}deg);
-  transform-origin: bottom;
-  display: flex;
-
-  &::after {
-    content: '';
-    position: absolute;
-    left: 0;
-    bottom: 0;
-    width: 100%;
-    height: ${TOTTER_LINE_HEIGHT}px;
-    background-color: red;
-  }
-`;
-
-const DoneItemsContainerDiv = styled.div`
-  width: 50%;
-  height: 100%;
-  position: relative;
-`;
-
-const TriangleDiv = styled.div`
-  width: 0;
-  height: 0;
-  border-left: ${TOTTER_BALANCER_HEIGHT / 3}px solid transparent;
-  border-right: ${TOTTER_BALANCER_HEIGHT / 3}px solid transparent;
-  border-bottom: ${TOTTER_BALANCER_HEIGHT}px solid gray;
 `;
 
 const ItemViewContainerDiv = styled.div`
@@ -101,8 +58,6 @@ const GameBoardView = () => {
     hasReachedGoalLine,
     speedLevel,
     ongoingItems,
-    doneItems,
-    torque,
   } = useAppSelector(selectGame);
   const dispatch = useAppDispatch();
   const fallingItemRef = React.useRef<HTMLDivElement>(null);
@@ -195,43 +150,7 @@ const GameBoardView = () => {
           />
         )}
       </ItemViewContainerDiv>
-      <TotterBaseDiv>
-        <TotterLineDiv torque={torque}>
-          <DoneItemsContainerDiv>
-            {doneItems?.human.map((d, idx) => (
-              <ItemView
-                // TODO(emrerdem1): You should consider having ID
-                //or something equivalent for sorting etc.
-                key={idx + d.offsetX + d.offsetY}
-                weight={d.weight}
-                scaleSize={d.scaleSize}
-                offsetX={d.offsetX}
-                offsetY={d.offsetY}
-                itemShape={d.itemShape}
-                itemColor={d.itemColor}
-                hasReachedGoalLine={true}
-              />
-            ))}
-          </DoneItemsContainerDiv>
-          <DoneItemsContainerDiv>
-            {doneItems?.machine.map((d, idx) => (
-              <ItemView
-                // TODO(emrerdem1): You should consider having ID
-                //or something equivalent for sorting etc.
-                key={idx + d.offsetX + d.offsetY}
-                weight={d.weight}
-                scaleSize={d.scaleSize}
-                offsetX={d.offsetX}
-                offsetY={d.offsetY}
-                itemShape={d.itemShape}
-                itemColor={d.itemColor}
-                hasReachedGoalLine={true}
-              />
-            ))}
-          </DoneItemsContainerDiv>
-        </TotterLineDiv>
-        <TriangleDiv></TriangleDiv>
-      </TotterBaseDiv>
+      <BalancerView />
     </GameBoardDiv>
   );
 };
